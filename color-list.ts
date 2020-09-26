13beaec6c4a93392c4d6fd0f32b6bad93b98d328
Amin Kiani <aminkiani.pazh@gmail.com>
2020-09-26 19:26:57 +0330

    add color list

---
+++ color-list.ts
@@ -0,0 +1,920 @@
+export const colorListObject: ColorTagListObjetInterface = {
+  white: {
+    _id: 'white',
+    name: {
+      fa: 'سفید',
+      en: 'White'
+    },
+    hex: 'FFFFFF'
+  },
+  red: {
+    _id: 'red',
+    name: {
+      fa: 'قرمز',
+      en: 'Red'
+    },
+    hex: 'FF0000',
+    colorListArray: [
+      {
+        name: {
+          fa: '',
+          en: 'Indian Red'
+        },
+        hex: 'CD5C5C'
+      },
+      {
+        name: {
+          fa: '',
+          en: 'Light Coral'
+        },
+        hex: 'F08080'
+      },
+      {
+        name: {
+          fa: '',
+          en: 'Salmon'
+        },
+        hex: 'FA8072'
+      },
+      {
+        name: {
+          fa: '',
+          en: 'Dark Salmon'
+        },
+        hex: 'E9967A'
+      },
+      {
+        name: {
+          fa: '',
+          en: 'Light Salmon'
+        },
+        hex: 'FFA07A'
+      },
+      {
+        name: {
+          fa: '',
+          en: 'Crimson'
+        },
+        hex: 'DC143C'
+      },
+      {
+        name: {
+          fa: '',
+          en: 'Fire Brick'
+        },
+        hex: 'B22222'
+      },
+      {
+        name: {
+          fa: '',
+          en: 'Dark Red'
+        },
+        hex: '8B0000'
+      }
+    ]
+  },
+  pink: {
+    _id: 'pink',
+    name: {
+      fa: 'صورتی',
+      en: 'Pink'
+    },
+    hex: 'FFC0CB',
+    colorListArray: [
+      {
+        name: {
+          fa: '',
+          en: 'Light Pink'
+        },
+        hex: 'FFB6C1'
+      },
+      {
+        name: {
+          fa: '',
+          en: 'Hot Pink'
+        },
+        hex: 'FF69B4'
+      },
+      {
+        name: {
+          fa: '',
+          en: 'Deep Pink'
+        },
+        hex: 'FF1493'
+      },
+      {
+        name: {
+          fa: '',
+          en: 'Medium Violet Red'
+        },
+        hex: 'C71585'
+      },
+      {
+        name: {
+          fa: '',
+          en: 'Pale Violet Red'
+        },
+        hex: 'DB7093'
+      }
+    ]
+  },
+  orange: {
+    _id: 'orange',
+    name: {
+      fa: 'نارنجی',
+      en: 'Orange'
+    },
+    hex: 'FFA500',
+    colorListArray: [
+      {
+        name: {
+          fa: '',
+          en: 'Light Salmon'
+        },
+        hex: 'FFA07A'
+      },
+      {
+        name: {
+          fa: '',
+          en: 'Coral'
+        },
+        hex: 'FF7F50'
+      },
+      {
+        name: {
+          fa: '',
+          en: 'Tomato'
+        },
+        hex: 'FF6347'
+      },
+      {
+        name: {
+          fa: '',
+          en: 'Orange Red'
+        },
+        hex: 'FF4500'
+      },
+      {
+        name: {
+          fa: '',
+          en: 'Dark Orange'
+        },
+        hex: 'FF8C00'
+      }
+    ]
+  },
+  yellow: {
+    _id: 'yellow',
+    name: {
+      fa: 'زرد',
+      en: 'Yellow'
+    },
+    hex: 'FFFF00',
+    colorListArray: [
+      {
+        name: {
+          fa: '',
+          en: 'Gold'
+        },
+        hex: 'FFD700'
+      },
+      {
+        name: {
+          fa: '',
+          en: 'Light Yellow'
+        },
+        hex: 'FFFFE0'
+      },
+      {
+        name: {
+          fa: '',
+          en: 'Lemon Chiffon'
+        },
+        hex: 'FFFACD'
+      },
+      {
+        name: {
+          fa: '',
+          en: 'Light Goldenrod Yellow'
+        },
+        hex: 'FAFAD2'
+      },
+      {
+        name: {
+          fa: '',
+          en: 'Papaya Whip'
+        },
+        hex: 'FFEFD5'
+      },
+      {
+        name: {
+          fa: '',
+          en: 'Moccasin'
+        },
+        hex: 'FFE4B5'
+      },
+      {
+        name: {
+          fa: '',
+          en: 'Peach Puff'
+        },
+        hex: 'FFDAB9'
+      },
+      {
+        name: {
+          fa: '',
+          en: 'Pale Goldenrod'
+        },
+        hex: 'EEE8AA'
+      },
+      {
+        name: {
+          fa: '',
+          en: 'Khaki'
+        },
+        hex: 'F0E68C'
+      },
+      {
+        name: {
+          fa: '',
+          en: 'Dark Khaki'
+        },
+        hex: 'BDB76B'
+      }
+    ]
+  },
+  purple: {
+    _id: 'purple',
+    name: {
+      fa: 'بنفش',
+      en: 'Purple'
+    },
+    hex: '800080',
+    colorListArray: [
+      {
+        name: {
+          fa: '',
+          en: 'Lavender'
+        },
+        hex: 'E6E6FA'
+      },
+      {
+        name: {
+          fa: '',
+          en: 'Thistle'
+        },
+        hex: 'D8BFD8'
+      },
+      {
+        name: {
+          fa: '',
+          en: 'Plum'
+        },
+        hex: 'DDA0DD'
+      },
+      {
+        name: {
+          fa: '',
+          en: 'Violet'
+        },
+        hex: 'EE82EE'
+      },
+      {
+        name: {
+          fa: '',
+          en: 'Orchid'
+        },
+        hex: 'DA70D6'
+      },
+      {
+        name: {
+          fa: '',
+          en: 'Fuchsia'
+        },
+        hex: 'FF00FF'
+      },
+      {
+        name: {
+          fa: '',
+          en: 'Magenta'
+        },
+        hex: 'FF00FF'
+      },
+      {
+        name: {
+          fa: '',
+          en: 'Medium Orchid'
+        },
+        hex: 'BA55D3'
+      },
+      {
+        name: {
+          fa: '',
+          en: 'Medium Purple'
+        },
+        hex: '9370DB'
+      },
+      {
+        name: {
+          fa: '',
+          en: 'Rebecca Purple'
+        },
+        hex: '663399'
+      },
+      {
+        name: {
+          fa: '',
+          en: 'Blue Violet'
+        },
+        hex: '8A2BE2'
+      },
+      {
+        name: {
+          fa: '',
+          en: 'Dark Violet'
+        },
+        hex: '9400D3'
+      },
+      {
+        name: {
+          fa: '',
+          en: 'Dark Orchid'
+        },
+        hex: '9932CC'
+      },
+      {
+        name: {
+          fa: '',
+          en: 'Dark Magenta'
+        },
+        hex: '8B008B'
+      },
+      {
+        name: {
+          fa: '',
+          en: 'Indigo'
+        },
+        hex: '4B0082'
+      },
+      {
+        name: {
+          fa: '',
+          en: 'Slate Blue'
+        },
+        hex: '6A5ACD'
+      },
+      {
+        name: {
+          fa: '',
+          en: 'Dark Slate Blue'
+        },
+        hex: '483D8B'
+      },
+      {
+        name: {
+          fa: '',
+          en: 'Medium Slate Blue'
+        },
+        hex: '7B68EE'
+      }
+    ]
+  },
+  green: {
+    _id: 'green',
+    name: {
+      fa: 'سبز',
+      en: 'Green'
+    },
+    hex: '008000',
+    colorListArray: [
+      {
+        name: {
+          fa: '',
+          en: 'Green Yellow'
+        },
+        hex: 'ADFF2F'
+      },
+      {
+        name: {
+          fa: '',
+          en: 'Chartreuse'
+        },
+        hex: '7FFF00'
+      },
+      {
+        name: {
+          fa: '',
+          en: 'Lawn Green'
+        },
+        hex: '7CFC00'
+      },
+      {
+        name: {
+          fa: '',
+          en: 'Lime'
+        },
+        hex: '00FF00'
+      },
+      {
+        name: {
+          fa: '',
+          en: 'Lime Green'
+        },
+        hex: '32CD32'
+      },
+      {
+        name: {
+          fa: '',
+          en: 'Pale Green'
+        },
+        hex: '98FB98'
+      },
+      {
+        name: {
+          fa: '',
+          en: 'Light Green'
+        },
+        hex: '90EE90'
+      },
+      {
+        name: {
+          fa: '',
+          en: 'Medium Spring Green'
+        },
+        hex: '00FA9A'
+      },
+      {
+        name: {
+          fa: '',
+          en: 'Spring Green'
+        },
+        hex: '00FF7F'
+      },
+      {
+        name: {
+          fa: '',
+          en: 'Medium Sea Green'
+        },
+        hex: '3CB371'
+      },
+      {
+        name: {
+          fa: '',
+          en: 'Sea Green'
+        },
+        hex: '2E8B57'
+      },
+      {
+        name: {
+          fa: '',
+          en: 'Forest Green'
+        },
+        hex: '228B22'
+      },
+      {
+        name: {
+          fa: '',
+          en: 'Dark Green'
+        },
+        hex: '006400'
+      },
+      {
+        name: {
+          fa: '',
+          en: 'Yellow Green'
+        },
+        hex: '9ACD32'
+      },
+      {
+        name: {
+          fa: '',
+          en: 'Olive Drab'
+        },
+        hex: '6B8E23'
+      },
+      {
+        name: {
+          fa: '',
+          en: 'Olive'
+        },
+        hex: '808000'
+      },
+      {
+        name: {
+          fa: '',
+          en: 'Dark Olive Green'
+        },
+        hex: '556B2F'
+      },
+      {
+        name: {
+          fa: '',
+          en: 'Medium Aquamarine'
+        },
+        hex: '66CDAA'
+      },
+      {
+        name: {
+          fa: '',
+          en: 'Dark Sea Green'
+        },
+        hex: '8FBC8B'
+      },
+      {
+        name: {
+          fa: '',
+          en: 'Light Sea Green'
+        },
+        hex: '20B2AA'
+      },
+      {
+        name: {
+          fa: '',
+          en: 'Dark Cyan'
+        },
+        hex: '008B8B'
+      },
+      {
+        name: {
+          fa: '',
+          en: 'Teal'
+        },
+        hex: '008080'
+      }
+    ]
+  },
+  blue: {
+    _id: 'blue',
+    name: {
+      fa: 'آبی',
+      en: 'Blue'
+    },
+    hex: '0000FF',
+    colorListArray: [
+      {
+        name: {
+          fa: '',
+          en: 'Aqua'
+        },
+        hex: '00FFFF'
+      },
+      {
+        name: {
+          fa: '',
+          en: 'Cyan'
+        },
+        hex: '00FFFF'
+      },
+      {
+        name: {
+          fa: '',
+          en: 'Light Cyan'
+        },
+        hex: 'E0FFFF'
+      },
+      {
+        name: {
+          fa: '',
+          en: 'Pale Turquoise'
+        },
+        hex: 'AFEEEE'
+      },
+      {
+        name: {
+          fa: '',
+          en: 'Aquamarine'
+        },
+        hex: '7FFFD4'
+      },
+      {
+        name: {
+          fa: '',
+          en: 'Turquoise'
+        },
+        hex: '40E0D0'
+      },
+      {
+        name: {
+          fa: '',
+          en: 'Medium Turquoise'
+        },
+        hex: '48D1CC'
+      },
+      {
+        name: {
+          fa: '',
+          en: 'Dark Turquoise'
+        },
+        hex: '00CED1'
+      },
+      {
+        name: {
+          fa: '',
+          en: 'Cadet Blue'
+        },
+        hex: '5F9EA0'
+      },
+      {
+        name: {
+          fa: '',
+          en: 'Steel Blue'
+        },
+        hex: '4682B4'
+      },
+      {
+        name: {
+          fa: '',
+          en: 'Light Steel Blue'
+        },
+        hex: 'B0C4DE'
+      },
+      {
+        name: {
+          fa: '',
+          en: 'Powder Blue'
+        },
+        hex: 'B0E0E6'
+      },
+      {
+        name: {
+          fa: '',
+          en: 'Light Blue'
+        },
+        hex: 'ADD8E6'
+      },
+      {
+        name: {
+          fa: '',
+          en: 'Sky Blue'
+        },
+        hex: '87CEEB'
+      },
+      {
+        name: {
+          fa: '',
+          en: 'Light Sky Blue'
+        },
+        hex: '87CEFA'
+      },
+      {
+        name: {
+          fa: '',
+          en: 'Deep Sky Blue'
+        },
+        hex: '00BFFF'
+      },
+      {
+        name: {
+          fa: '',
+          en: 'Dodger Blue'
+        },
+        hex: '1E90FF'
+      },
+      {
+        name: {
+          fa: '',
+          en: 'Corn Flower Blue'
+        },
+        hex: '6495ED'
+      },
+      {
+        name: {
+          fa: '',
+          en: 'Medium Slate Blue'
+        },
+        hex: '7B68EE'
+      },
+      {
+        name: {
+          fa: '',
+          en: 'Royal Blue'
+        },
+        hex: '4169E1'
+      },
+      {
+        name: {
+          fa: '',
+          en: 'Medium Blue'
+        },
+        hex: '0000CD'
+      },
+      {
+        name: {
+          fa: '',
+          en: 'Dark Blue'
+        },
+        hex: '00008B'
+      },
+      {
+        name: {
+          fa: '',
+          en: 'Navy'
+        },
+        hex: '000080'
+      },
+      {
+        name: {
+          fa: '',
+          en: 'Midnight Blue'
+        },
+        hex: '191970'
+      }
+    ]
+  },
+  brown: {
+    _id: 'brown',
+    name: {
+      fa: 'قهوه ای',
+      en: 'Brown'
+    },
+    hex: 'A52A2A',
+    colorListArray: [
+      {
+        name: {
+          fa: '',
+          en: 'Cornsilk'
+        },
+        hex: 'FFF8DC'
+      },
+      {
+        name: {
+          fa: '',
+          en: 'Blanched almond'
+        },
+        hex: 'FFEBCD'
+      },
+      {
+        name: {
+          fa: '',
+          en: 'Bisque'
+        },
+        hex: 'FFE4C4'
+      },
+      {
+        name: {
+          fa: '',
+          en: 'Navajo white'
+        },
+        hex: 'FFDEAD'
+      },
+      {
+        name: {
+          fa: '',
+          en: 'Wheat'
+        },
+        hex: 'F5DEB3'
+      },
+      {
+        name: {
+          fa: '',
+          en: 'Burly wood'
+        },
+        hex: 'DEB887'
+      },
+      {
+        name: {
+          fa: '',
+          en: 'Tan'
+        },
+        hex: 'D2B48C'
+      },
+      {
+        name: {
+          fa: '',
+          en: 'Rosy brown'
+        },
+        hex: 'BC8F8F'
+      },
+      {
+        name: {
+          fa: '',
+          en: 'Sandy brown'
+        },
+        hex: 'F4A460'
+      },
+      {
+        name: {
+          fa: '',
+          en: 'Goldenrod'
+        },
+        hex: 'DAA520'
+      },
+      {
+        name: {
+          fa: '',
+          en: 'Dark goldenrod'
+        },
+        hex: 'B8860B'
+      },
+      {
+        name: {
+          fa: '',
+          en: 'Peru'
+        },
+        hex: 'CD853F'
+      },
+      {
+        name: {
+          fa: '',
+          en: 'Chocolate'
+        },
+        hex: 'D2691E'
+      },
+      {
+        name: {
+          fa: '',
+          en: 'Saddle brown'
+        },
+        hex: '8B4513'
+      },
+      {
+        name: {
+          fa: '',
+          en: 'Sienna'
+        },
+        hex: 'A0522D'
+      },
+      {
+        name: {
+          fa: '',
+          en: 'Maroon'
+        },
+        hex: '800000'
+      }
+    ]
+  },
+  gray: {
+    _id: 'gray',
+    name: {
+      fa: 'خاکستری',
+      en: 'Gray'
+    },
+    hex: '808080',
+    colorListArray: [
+      {
+        name: {
+          fa: '',
+          en: 'Gainsboro'
+        },
+        hex: 'DCDCDC'
+      },
+      {
+        name: {
+          fa: '',
+          en: 'Light Gray'
+        },
+        hex: 'D3D3D3'
+      },
+      {
+        name: {
+          fa: '',
+          en: 'Silver'
+        },
+        hex: 'C0C0C0'
+      },
+      {
+        name: {
+          fa: '',
+          en: 'Dark Gray'
+        },
+        hex: 'A9A9A9'
+      },
+      {
+        name: {
+          fa: '',
+          en: 'Dim Gray'
+        },
+        hex: '696969'
+      },
+      {
+        name: {
+          fa: '',
+          en: 'Light Slate Gray'
+        },
+        hex: '778899'
+      },
+      {
+        name: {
+          fa: '',
+          en: 'Slate Gray'
+        },
+        hex: '708090'
+      },
+      {
+        name: {
+          fa: '',
+          en: 'Dark Slate Gray'
+        },
+        hex: '2F4F4F'
+      }
+    ]
+  },
+  black: {
+    _id: 'black',
+    name: {
+      fa: 'مشکی',
+      en: 'Black'
+    },
+    hex: '000000'
+  }
+};

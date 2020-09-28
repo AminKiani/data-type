0ee1983bd72ef366be87b3ef3e792653d807ee3e
Amin Kiani <aminkiani.pazh@gmail.com>
2020-09-28 10:27:45 +0330

    edit city

--- country-list.ts
+++ country-list.ts
@@ -3,7 +3,6 @@ import { CountryInterface } from '../base/country';
 export interface CountryListObjectInterface {
   ir: CountryInterface;
   us: CountryInterface;
-  en: CountryInterface;
   ae: CountryInterface;
   tr: CountryInterface;
 }
@@ -22,19 +21,522 @@ export const countryListObject: CountryListObjectInterface = {
     direction: 'rtl',
     stateList: [
       {
-        _id: 'khr',
-        globalName: 'Khorasan Razavi',
-        localName: 'رضوی خراسان',
+        _id: 'eastAzerbaijan',
+        globalName: 'East Azerbaijan',
+        localName: 'آذربايجان شرقی',
         cityList: [
           {
             _id: '',
             globalName: '',
             localName: '',
-            phonePrefix: '51',
+            phonePrefix: '',
           }
         ]
-      }
-    ],
+      },
+      {
+        _id: 'westAzerbaijan',
+        globalName: 'West Azerbaijan',
+        localName: 'آذربایجان غربی',
+        cityList: [
+          {
+            _id: '',
+            globalName: '',
+            localName: '',
+            phonePrefix: '',
+          }
+        ]
+      },
+      {
+        _id: '3',
+        globalName: 'Ardabil',
+        localName: 'اردبیل',
+        cityList: [
+          {
+            _id: '',
+            globalName: '',
+            localName: '',
+            phonePrefix: '',
+          }
+        ]
+      },
+      {
+        _id: '4',
+        globalName: 'Isfahan',
+        localName: 'اصفهان',
+        cityList: [
+          {
+            _id: '',
+            globalName: '',
+            localName: '',
+            phonePrefix: '',
+          }
+        ]
+      },
+      {
+        _id: '5',
+        globalName: 'Alborz',
+        localName: 'البرز',
+        cityList: [
+          {
+            _id: '',
+            globalName: '',
+            localName: '',
+            phonePrefix: '',
+          }
+        ]
+      },
+      {
+        _id: '6',
+        globalName: 'Ilam',
+        localName: 'ایلام',
+        cityList: [
+          {
+            _id: '',
+            globalName: '',
+            localName: '',
+            phonePrefix: '',
+          }
+        ]
+      },
+      {
+        _id: '7',
+        globalName: 'Bushehr',
+        localName: 'بوشهر',
+        cityList: [
+          {
+            _id: '',
+            globalName: '',
+            localName: '',
+            phonePrefix: '',
+          }
+        ]
+      },
+      {
+        _id: '8',
+        globalName: 'Tehran',
+        localName: 'تهران',
+        cityList: [
+          {
+            _id: '',
+            globalName: '',
+            localName: '',
+            phonePrefix: '',
+          }
+        ]
+      },
+      {
+        _id: '9',
+        globalName: 'South Khorasan',
+        localName: 'خراسان جنوبی',
+        cityList: [
+          {
+            _id: '',
+            globalName: '',
+            localName: '',
+            phonePrefix: '',
+          }
+        ]
+      },
+      {
+        _id: 'khorasanRazavi',
+        globalName: 'Khorasan Razavi',
+        localName: 'خراسان رضوی',
+        cityList: [
+          {
+            _id: 'bajestan',
+            localName: 'بجستان',
+            globalName: 'Bajestan',
+          },
+          {
+            _id: 'bardaskan',
+            localName: 'بردسكن',
+            globalName: 'Bardaskan',
+          },
+          {
+            _id: 'taybad',
+            localName: 'تايباد',
+            globalName: 'Taybad',
+          },
+          {
+            _id: 'torbat-e jam',
+            localName: 'تربت جام',
+            globalName: 'Torbat-e Jam',
+          },
+          {
+            _id: 'torbat',
+            localName: 'تربت حيدريه',
+            globalName: 'Torbat',
+          },
+          {
+            _id: 'jaghatay',
+            localName: 'جغتاي',
+            globalName: 'Jaghatay',
+          },
+          {
+            _id: 'jovin',
+            localName: 'جوين',
+            globalName: 'Jovin',
+          },
+          {
+            _id: 'khaliabad',
+            localName: 'خليل آباد',
+            globalName: 'Khaliabad',
+          },
+          {
+            _id: 'khaf',
+            localName: 'خواف',
+            globalName: 'KHAF',
+          },
+          {
+            _id: 'davarzan',
+            localName: 'داورزن',
+            globalName: 'Davarzan',
+          },
+          {
+            _id: 'dargaz',
+            localName: 'درگز',
+            globalName: 'Dargaz',
+          },
+          {
+            _id: 'roshtkhar',
+            localName: 'رشتخوار',
+            globalName: 'Roshtkhar',
+          },
+          {
+            _id: 'zavh',
+            localName: 'زاوه',
+            globalName: 'Zavh',
+          },
+          {
+            _id: 'sabzevar',
+            localName: 'سبزوار',
+            globalName: 'Sabzevar',
+          },
+          {
+            _id: 'fern',
+            localName: 'سرخس',
+            globalName: 'Fern',
+          },
+          {
+            _id: 'fariman',
+            localName: 'فريمان',
+            globalName: 'FARIMAN',
+          },
+          {
+            _id: 'ghoochan',
+            localName: 'قوچان',
+            globalName: 'Ghoochan',
+          },
+          {
+            _id: 'kashmar',
+            localName: 'كاشمر',
+            globalName: 'Kashmar',
+          },
+          {
+            _id: 'kalat',
+            localName: 'كلات',
+            globalName: 'Kalat',
+          },
+          {
+            _id: 'mashhad',
+            localName: 'مشهد',
+            globalName: 'Mashhad',
+          },
+          {
+            _id: 'mahvelat',
+            localName: 'مه ولات',
+            globalName: 'Mahvelat',
+          },
+          {
+            _id: 'neyshabur',
+            localName: 'نيشابور',
+            globalName: 'Neyshabur',
+          },
+          {
+            _id: 'chenaran',
+            localName: 'چناران',
+            globalName: 'CHENARAN',
+          },
+          {
+            _id: 'gonabad',
+            localName: 'گناباد',
+            globalName: 'GONABAD',
+          }
+        ]
+      },
+      {
+        _id: '11',
+        globalName: 'North Khorasan',
+        localName: 'خراسان شمالی',
+        cityList: [
+          {
+            _id: '',
+            globalName: '',
+            localName: '',
+            phonePrefix: '',
+          }
+        ]
+      },
+      {
+        _id: '12',
+        globalName: 'Khuzestan',
+        localName: 'خوزستان',
+        cityList: [
+          {
+            _id: '',
+            globalName: '',
+            localName: '',
+            phonePrefix: '',
+          }
+        ]
+      },
+      {
+        _id: '13',
+        globalName: 'Zanjan',
+        localName: 'زنجان',
+        cityList: [
+          {
+            _id: '',
+            globalName: '',
+            localName: '',
+            phonePrefix: '',
+          }
+        ]
+      },
+      {
+        _id: '14',
+        globalName: 'Semnan',
+        localName: 'سمنان',
+        cityList: [
+          {
+            _id: '',
+            globalName: '',
+            localName: '',
+            phonePrefix: '',
+          }
+        ]
+      },
+      {
+        _id: '15',
+        globalName: 'Sistan and Baluchestan',
+        localName: 'سیستان و بلوچستان',
+        cityList: [
+          {
+            _id: '',
+            globalName: '',
+            localName: '',
+            phonePrefix: '',
+          }
+        ]
+      },
+      {
+        _id: '16',
+        globalName: 'Fars',
+        localName: 'فارس',
+        cityList: [
+          {
+            _id: '',
+            globalName: '',
+            localName: '',
+            phonePrefix: '',
+          }
+        ]
+      },
+      {
+        _id: '17',
+        globalName: 'Qazvin',
+        localName: 'قزوین',
+        cityList: [
+          {
+            _id: '',
+            globalName: '',
+            localName: '',
+            phonePrefix: '',
+          }
+        ]
+      },
+      {
+        _id: '18',
+        globalName: 'Qom',
+        localName: 'قم',
+        cityList: [
+          {
+            _id: '',
+            globalName: '',
+            localName: '',
+            phonePrefix: '',
+          }
+        ]
+      },
+      {
+        _id: '19',
+        globalName: 'Kurdistan',
+        localName: 'کردستان',
+        cityList: [
+          {
+            _id: '',
+            globalName: '',
+            localName: '',
+            phonePrefix: '',
+          }
+        ]
+      },
+      {
+        _id: '20',
+        globalName: 'Kerman',
+        localName: 'کرمان',
+        cityList: [
+          {
+            _id: '',
+            globalName: '',
+            localName: '',
+            phonePrefix: '',
+          }
+        ]
+      },
+      {
+        _id: '21',
+        globalName: 'Kermanshah',
+        localName: 'کرمانشاه',
+        cityList: [
+          {
+            _id: '',
+            globalName: '',
+            localName: '',
+            phonePrefix: '',
+          }
+        ]
+      },
+      {
+        _id: '22',
+        globalName: 'Kohgiluyeh and Boyer-Ahmad',
+        localName: 'كهگيلويه و بويراحمد',
+        cityList: [
+          {
+            _id: '',
+            globalName: '',
+            localName: '',
+            phonePrefix: '',
+          }
+        ]
+      },
+      {
+        _id: '23',
+        globalName: 'Golestan',
+        localName: 'گلستان',
+        cityList: [
+          {
+            _id: '',
+            globalName: '',
+            localName: '',
+            phonePrefix: '',
+          }
+        ]
+      },
+      {
+        _id: '24',
+        globalName: 'Gilan',
+        localName: 'گیلان',
+        cityList: [
+          {
+            _id: '',
+            globalName: '',
+            localName: '',
+            phonePrefix: '',
+          }
+        ]
+      },
+      {
+        _id: '25',
+        globalName: 'Lorestan',
+        localName: 'لرستان',
+        cityList: [
+          {
+            _id: '',
+            globalName: '',
+            localName: '',
+            phonePrefix: '',
+          }
+        ]
+      },
+      {
+        _id: '26',
+        globalName: 'Mazandaran',
+        localName: 'مازندران',
+        cityList: [
+          {
+            _id: '',
+            globalName: '',
+            localName: '',
+            phonePrefix: '',
+          }
+        ]
+      },
+      {
+        _id: '27',
+        globalName: 'Markazi',
+        localName: 'مرکزی',
+        cityList: [
+          {
+            _id: '',
+            globalName: '',
+            localName: '',
+            phonePrefix: '',
+          }
+        ]
+      },
+      {
+        _id: '28',
+        globalName: 'Hormozgan',
+        localName: 'هرمزگان',
+        cityList: [
+          {
+            _id: '',
+            globalName: '',
+            localName: '',
+            phonePrefix: '',
+          }
+        ]
+      },
+      {
+        _id: '29',
+        globalName: 'Hamadan',
+        localName: 'همدان',
+        cityList: [
+          {
+            _id: '',
+            globalName: '',
+            localName: '',
+            phonePrefix: '',
+          }
+        ]
+      },
+      {
+        _id: '30',
+        globalName: 'Chahar Mahaal and Bakhtiari',
+        localName: 'چهارمحال و بختیاری',
+        cityList: [
+          {
+            _id: '',
+            globalName: '',
+            localName: '',
+            phonePrefix: '',
+          }
+        ]
+      },
+      {
+        _id: '31',
+        globalName: 'Yazd',
+        localName: 'یزد',
+        cityList: [
+          {
+            _id: '',
+            globalName: '',
+            localName: '',
+            phonePrefix: '',
+          }
+        ]
+      },
     flag: {
       alt: {
         en: 'Iran',
@@ -42,7 +544,7 @@ export const countryListObject: CountryListObjectInterface = {
       },
       uri: 'https://res.cloudinary.com/pazhnet/image/upload/v1571645755/image/svg-flag/ir_z7z8k6.svg'
     }
-  },
+  }
   us: {
     _id: 'us',
     globalName: 'United States of America',
@@ -75,40 +577,6 @@ export const countryListObject: CountryListObjectInterface = {
         fa: 'ایالات متحده آمریکا'
       },
       uri: 'https://res.cloudinary.com/pazhnet/image/upload/v1571645795/image/us-flag_o5kqjp.png'
-    }
-  },
-  en: {
-    _id: 'en',
-    globalName: 'United Kingdom',
-    localName: 'United Kingdom',
-    language: {
-      _id: 'en',
-      globalName: 'English',
-      localName: 'English',
-    },
-    phonePrefix: '+1',
-    direction: 'ltr',
-    stateList: [
-      {
-        _id: 'lo',
-        globalName: 'London',
-        localName: 'London',
-        cityList: [
-          {
-            _id: 'lo',
-            globalName: 'London',
-            localName: 'London',
-            phonePrefix: '2',
-          }
-        ]
-      }
-    ],
-    flag: {
-      alt: {
-        en: 'United Kingdom',
-        fa: 'انگلستان'
-      },
-      uri: 'https://res.cloudinary.com/pazhnet/image/upload/v1571645727/image/svg-flag/gb_l4fcn9.svg'
     }
   },
   ae: {
